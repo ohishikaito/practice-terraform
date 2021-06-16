7ab872ff4b46736e6615f7f92a0faa64793d0403
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-17 07:42:15 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "example" {
+  ami = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+  tags = {
+    "Name" = "value"
+  }
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+}
+# ap-northeast-1
