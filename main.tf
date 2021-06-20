ddd37a2cb5453aa2ccc3f5b4c7c3eccad70995f5
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-20 09:26:04 +0900

    9.3

--- main.tf
+++ main.tf
@@ -87,3 +87,73 @@ resource "aws_s3_bucket" "force_destroy" {
   bucket = "force-destroy-pragmatic-terraform-ohishikaito20210618"
   force_destroy = true
 }
+
+resource "aws_ecs_cluster" "example" {
+  name = "example"
+}
+
+resource "aws_ecs_task_definition" "example" {
+  family = "example"
+  cpu = "256"
+  memory = "512"
+  network_mode = "awsvpc"
+  requires_compatibilities = [ "FARGATE" ]
+  container_definitions = file("./task_definitions.json")
+}
+
+resource "aws_ecs_service" "example" {
+  name = "example"
+  cluster = aws_ecs_cluster.example.arn
+  task_definition = aws_ecs_task_definition.example.arn
+  desired_count = 2
+  launch_type = "FARGATE"
+  platform_version = "1.3.0"
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups = [module.nginx_sg.security_group_id]
+
+    subnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name = "example"
+    container_port = 80
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+module "nginx_sg" {
+  source = "./security_group"
+  name = "nginx-sg"
+  vpc_id = aws_vpc.example.id
+  port = 80
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
+# resource "aws_cloudwatch_log_group" "for_ecs" {
+#   name = "/ecs/example"
+#   retention_in_days = 180
+# }
+
+# data "aws_iam_policy" "ecs_task_execution_role_policy" {
+#   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
+
+# data "aws_iam_policy_document" "ecs_task_execution" {
+#   source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+#   statement {
+#     effect = "Allow"
+#     actions = ["ssm:GetParameters", "kms:Decrypt"]
+#     resouces = ["*"]
+#   }
+# }
